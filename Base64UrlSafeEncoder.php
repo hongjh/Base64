b13d68ce89371e2d6a6daf380f7342abb8244f05
hongjh <565983236@qq.com>
2016-07-22 10:54:03 +0800

    增加base64 封装类

---
+++ Base64UrlSafeEncoder.php
@@ -0,0 +1,16 @@
+<?php
+
+class Base64UrlSafeEncoder implements Encoder
+{
+
+    public function encode($data)
+    {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+    }
+
+    public function decode($data)
+    {
+        return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+    }
+
+}
---
+++ Encoder.php
@@ -0,0 +1,19 @@
+<?php
+
+interface Encoder
+{
+
+    /**
+     * @param string $data
+     *
+     * @return string
+     */
+    public function encode($data);
+
+    /**
+     * @param string $data
+     *
+     * @return string
+     */
+    public function decode($data);
+}
